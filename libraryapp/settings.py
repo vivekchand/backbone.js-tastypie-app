4ba99dd793e79f895678793779e0c454c3e7c202
Vivek Chand <vivekchand19@gmail.com>
2014-03-23 15:41:05 +0530

    app ready to work on heroku

--- libraryapp/settings.py
+++ libraryapp/settings.py
@@ -64,6 +64,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+if dj_database_url.config():
+    DATABASES['default'] =  dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
