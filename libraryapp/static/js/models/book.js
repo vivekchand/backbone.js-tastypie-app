d5c77f179e36e77ca380e61d2d94340caf15d13f
Vivek Chand <vivekchand19@gmail.com>
2014-03-23 15:56:49 +0530

    rename defaults release_date

--- libraryapp/static/js/models/book.js
+++ libraryapp/static/js/models/book.js
@@ -4,7 +4,7 @@ app.Book = Backbone.Model.extend({
     defaults: {
         title: 'No title',
         author: 'Unknown',
-        releaseDate: 'Unknown',
+        release_date: 'Unknown',
     }
 });
 
